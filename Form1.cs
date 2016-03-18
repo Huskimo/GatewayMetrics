ba29c3edb6498958d3e20c205d05196c25262ad2
Rhys Howard <Rhys.Howard@egress.com>
2016-03-18 11:16:01 +0000

    restructure

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -36,15 +36,16 @@ namespace WindowsFormsApplication1
 
             string[] dirs = Directory.GetFiles(@"C:\\Users\\hasaan.ausat\\Desktop\\Logs");
 
-            //Saving the details' from each block of 1180 
-            //List<string> tempBlockDetails = new List<string>();
-            String tempBlockDetails = "";
+       	    //assigning temp variables for each line
             String tempDate = "";
             String tempTime = "";
             String tempId = "";
             String tempType = "";
             String tempDetails = "";
+            
+            String tempBlockDetails = "";
 
+            //For every file do..
             for (int j = 0; j < fileCount; j++)
             {
                 //reading in from text file
@@ -53,37 +54,33 @@ namespace WindowsFormsApplication1
                 //getting line count of the text file
                 int logLineCount = File.ReadLines(dirs[j]).Count();
 
-
+                //On every line
                 for (int i = 0; i < logLineCount; i++)
                 {
-                    //saving text from file to variable
-                    string tempLogs = file.ReadLine();
-
-                    //getting length of each line
-                    int logLength = tempLogs.Length;
-                    int lastChar = logLength - 63;
-
-                    //assigning substrings to temp variables
-                    tempDate = tempLogs.Substring(0, 10);
-                    tempTime = tempLogs.Substring(11, 8);
-                    tempType = tempLogs.Substring(34, 11);
-                    tempId = tempLogs.Substring(54, 4);
-                    tempDetails = tempLogs.Substring(63, lastChar);
-
                     if (tempId == "1180")
                     {
+                        //getting line from file to variable
+                        string tempLogs = file.ReadLine();
 
+                        //getting length of each line
+                        int logLength = tempLogs.Length;
+                        int lastChar = logLength - 63;
+
+                        //assigning substrings to temp variables
+                        tempDate = tempLogs.Substring(0, 10);
+                        tempTime = tempLogs.Substring(11, 8);
+                        tempType = tempLogs.Substring(34, 11);
+                        tempId = tempLogs.Substring(54, 4);
+                        tempDetails = tempLogs.Substring(63, lastChar);
+                        
+                        //Creating global string w/ temp details concatted together
                         tempBlockDetails = (tempBlockDetails + tempDetails);
                         
-                        // MessageBox.Show(tempBlockDetails);
-                        //tempBlockDetails.Add(tempDetails);
-                        //MessageBox.Show(tempDetails);
+                        dataGridView1.Rows.Add(tempDate, tempTime, tempType, tempId, tempDetails);
                     } else {
                         break;
                     }
-                   
                 }
-                dataGridView1.Rows.Add(tempDate, tempTime, tempType, tempId, tempBlockDetails);
             }
         }
     }
