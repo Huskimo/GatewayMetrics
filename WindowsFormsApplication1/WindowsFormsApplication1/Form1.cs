2f5387d941d6f96548bef7f88c9edffc20d3330d
Huskimo <hasaanausat@hotmail.com>
2016-04-05 14:50:08 +0100

    Added extra error checking when importing files

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
 
 
 namespace WindowsFormsApplication1
@@ -48,128 +49,145 @@ namespace WindowsFormsApplication1
             //clears table at beginning to allow for any changes in the file while running (i.e acts as a refresh button)
             dataGridView1.Rows.Clear();
 
-            //file location on my Egress Laptop C:\\Users\\hasaan.ausat\\Desktop\\Logs
-            var fileCount = (from doc in Directory.EnumerateFiles(@"D:\\Hasaan\\Desktop\\Logs", "*.log", SearchOption.AllDirectories)
-                             select doc).Count();
-
-            if (fileCount == 0)
+            //get file path from user
+            String x = Interaction.InputBox("Please enter the file location of the logs", "File Location", "D:\\Hasaan\\Desktop\\Logs", -1, -1);
+           
+            //ensure file path exists, if not display error message
+            if (!Directory.Exists(x))
             {
-                MessageBox.Show("There are no log files to import");
+                MessageBox.Show("The path '"+x+"' does not exist. Please try again.", null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                importButton.Text = "Import Text Files";
+            }
+            else {
 
-            } else {
-
-                //changes text on button
-                importButton.Text = "Refresh";
-
-                string[] dirs = Directory.GetFiles(@"D:\\Hasaan\\Desktop\\Logs");
-
-                //assigning temp variables for each line
-                String tempDate = "";
-                String tempTime = "";
-                String tempTimeOut = "";
-                String tempSize = "";
-                String tempAttach = "";
-                String tempLabel = "";
-                String tempDir = "";
-                String tempStatus = "";
-                String tempId = "";
-                String tempType = "";
-                String tempDetails = "";
-
-                String tempBlockDetails = "";
-
-                List<String> fullDetails = new List<String>();
-
-                //List<String> allDetails = new List<String>();
-
-                List<log> logList = new List<log>();
-
-                List<log> concatDetails = new List<log>();
-
-                List<log> logList2 = new List<log>();
-
-                //For every file do..
-                for (int j = 0; j < fileCount; j++)
+                //file location on my Egress Laptop C:\\Users\\hasaan.ausat\\Desktop\\Logs C:\\Program Files\\Egress\\SDX\\logs
+                var fileCount = (from doc in Directory.EnumerateFiles(@x, "*.log", SearchOption.AllDirectories)
+                                 select doc).Count();
+                //display message if folder is empty or continue loading files in to program
+                if (fileCount == 0)
                 {
-                    //reading in from text file
-                    StreamReader file = new StreamReader(dirs[j]);
+                    MessageBox.Show("There are no log files to import");
 
-                    //getting line count of the text file
-                    int logLineCount = File.ReadLines(dirs[j]).Count();
+                }
+                else {
 
-                    //On every line
-                    for (int i = 0; i < logLineCount; i++)
+                    //changes text on button
+                    importButton.Text = "Refresh";
+
+                    string[] dirs = Directory.GetFiles(@x);
+
+                    //assigning temp variables for each line
+                    String tempDate = "";
+                    String tempTime = "";
+                    String tempTimeOut = "";
+                    String tempSize = "";
+                    String tempAttach = "";
+                    String tempLabel = "";
+                    String tempDir = "";
+                    String tempStatus = "";
+                    String tempId = "";
+                    String tempType = "";
+                    String tempDetails = "";
+
+                    String tempBlockDetails = "";
+
+                    List<String> fullDetails = new List<String>();
+
+                    //List<String> allDetails = new List<String>();
+
+                    List<log> logList = new List<log>();
+
+                    List<log> concatDetails = new List<log>();
+
+                    List<log> logList2 = new List<log>();
+
+                    //For every file do..
+                    for (int j = 0; j < fileCount; j++)
                     {
-                        //getting line from file to variable
-                        string tempLogs = file.ReadLine();
+                        //reading in from text file
+                        StreamReader file = new StreamReader(dirs[j]);
 
-                        //getting length of each line
-                        int logLength = tempLogs.Length;
-                        int lastChar = logLength - 63;
+                        //getting line count of the text file
+                        int logLineCount = File.ReadLines(dirs[j]).Count();
 
-                        //assigning substrings to temp variables
-                        tempDate = tempLogs.Substring(0, 10);
-                        tempTime = tempLogs.Substring(11, 8);
-                        tempType = tempLogs.Substring(34, 11);
-                        tempId = tempLogs.Substring(54, 4);
-                        tempDetails = tempLogs.Substring(63, lastChar);
-
-                        //creating log object and pushing it onto the logList
-                        log tempLogObject = new log { date = tempDate, time = tempTime, type = tempType, id = tempId, details = tempDetails };
-                        logList.Insert(0, tempLogObject);
-
-                        //ensuring only specific ID's are searched for and displayed
-                        if (tempId == "1180" /*|| tempId == "1112"*/)
+                        //On every line
+                        for (int i = 0; i < logLineCount; i++)
                         {
-                            //check if ID is the same as the last
-                            if (logList.Count == 1 || logList[0].id == logList[1].id)
+                            //getting line from file to variable
+                            string tempLogs = file.ReadLine();
+
+                            //getting length of each line
+                            int logLength = tempLogs.Length;
+                            int lastChar = logLength - 63;
+
+                            //assigning substrings to temp variables
+                            tempDate = tempLogs.Substring(0, 10);
+                            tempTime = tempLogs.Substring(11, 8);
+                            tempType = tempLogs.Substring(34, 11);
+                            tempId = tempLogs.Substring(54, 4);
+                            tempDetails = tempLogs.Substring(63, lastChar);
+
+                            //creating log object and pushing it onto the logList
+                            log tempLogObject = new log { date = tempDate, time = tempTime, type = tempType, id = tempId, details = tempDetails };
+                            logList.Insert(0, tempLogObject);
+
+                            //ensuring only specific ID's are searched for and displayed
+                            if (tempId == "1180" /*|| tempId == "1112"*/)
                             {
-                                //if so push on the list as objects are part of group
-                                concatDetails.Insert(0, tempLogObject);
-                            } else {
-                                //if not new group created
-                                concatDetails.Insert(0, tempLogObject);
-                                //previous group has finished - concats all the details
-                                for (int k = concatDetails.Count; k > 1; k--) {
-                                    tempBlockDetails = tempBlockDetails + concatDetails[k - 1].details;
+                                //check if ID is the same as the last
+                                if (logList.Count == 1 || logList[0].id == logList[1].id)
+                                {
+                                    //if so push on the list as objects are part of group
+                                    concatDetails.Insert(0, tempLogObject);
                                 }
-                                fullDetails.Insert(0, tempBlockDetails);
+                                else {
+                                    //if not new group created
+                                    concatDetails.Insert(0, tempLogObject);
+                                    //previous group has finished - concats all the details
+                                    for (int k = concatDetails.Count; k > 1; k--)
+                                    {
+                                        tempBlockDetails = tempBlockDetails + concatDetails[k - 1].details;
+                                    }
+                                    fullDetails.Insert(0, tempBlockDetails);
 
-                                //apply regex tests and assign output to temp variables
-                                tempSize = rSize.Match(fullDetails[0]).ToString();
-                                if (rAttach.Match(fullDetails[0]).ToString() == "") {
-                                    tempAttach = "No";
-                                } else {
-                                    tempAttach = "Yes";
+                                    //apply regex tests and assign output to temp variables
+                                    tempSize = rSize.Match(fullDetails[0]).ToString();
+                                    if (rAttach.Match(fullDetails[0]).ToString() == "")
+                                    {
+                                        tempAttach = "No";
+                                    }
+                                    else {
+                                        tempAttach = "Yes";
+                                    }
+                                    tempLabel = rLabel.Match(fullDetails[0]).ToString();
+                                    tempDir = rDir.Match(fullDetails[0]).ToString();
+                                    //tempStatus = rStatus.Match(fullDetails[0]).ToString();
+
+                                    log tempLogObject2 = new log { timeOut = tempTimeOut, size = tempSize, attach = tempAttach, label = tempLabel, direction = tempDir, status = tempStatus };
+                                    logList2.Insert(0, tempLogObject2);
+
+                                    //while (fullDetails.Count > 1) {
+                                    //display in the grid
+                                    dataGridView1.Rows.Add(concatDetails[0].date, concatDetails[0].time, null, null, logList2[0].size, logList2[0].attach, logList2[0].label, null, concatDetails[0].type, concatDetails[0].id, fullDetails[0]);
+                                    //clear temp variables for next group
+                                    //  }
+                                    tempBlockDetails = "";
+                                    concatDetails.Clear();
                                 }
-                                tempLabel = rLabel.Match(fullDetails[0]).ToString();
-                                tempDir = rDir.Match(fullDetails[0]).ToString();
-                                //tempStatus = rStatus.Match(fullDetails[0]).ToString();
-
-                                log tempLogObject2 = new log { timeOut = tempTimeOut, size = tempSize, attach = tempAttach, label = tempLabel, direction = tempDir, status = tempStatus };
-                                logList2.Insert(0, tempLogObject2);
-
-                                //while (fullDetails.Count > 1) {
-                                //display in the grid
-                                dataGridView1.Rows.Add(concatDetails[0].date, concatDetails[0].time, null, null, logList2[0].size, logList2[0].attach, logList2[0].label, null, concatDetails[0].type, concatDetails[0].id, fullDetails[0]);
-                                //clear temp variables for next group
-                                //  }
-                                tempBlockDetails = "";
-                                concatDetails.Clear();
                             }
-                        }
 
-                        /* for (int l = fullDetails.Count; l > 0; l--)
-                         {
-                             //display in the grid
-                             dataGridView1.Rows.Add(concatDetails[l].date, concatDetails[l].time, concatDetails[l].type, concatDetails[l].id, fullDetails[l]);
-                             //clear temp variables for next group
-                             tempBlockDetails = "";
-                             concatDetails.Clear();
-                         } */
+                            /* for (int l = fullDetails.Count; l > 0; l--)
+                             {
+                                 //display in the grid
+                                 dataGridView1.Rows.Add(concatDetails[l].date, concatDetails[l].time, concatDetails[l].type, concatDetails[l].id, fullDetails[l]);
+                                 //clear temp variables for next group
+                                 tempBlockDetails = "";
+                                 concatDetails.Clear();
+                             } */
+                        }
+                        //close the file after use
+                        file.Close();
                     }
-                    //close the file after use
-                    file.Close();
                 }
             }
         }
