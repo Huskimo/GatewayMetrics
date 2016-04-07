b7c8b43fe5dbeb2cfcea9f659940016b1447a29f
Huskimo <hasaanausat@hotmail.com>
2016-04-07 15:05:00 +0100

    more code tidying

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Text.RegularExpressions;
 using Microsoft.VisualBasic;
 using System.Net.Mail;
 using System.Net;
-using System.Net.Mime;
 
 namespace WindowsFormsApplication1
 {
